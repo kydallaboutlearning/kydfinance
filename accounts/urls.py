1d143d8197021a37ed46dcb98ad27c76cdecb909
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-01-24 00:08:31 +0100

    Chore(urls): Added the account urls but without path

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.utils.translation import gettext_lazy as _
+
+                    
+
+# URL patterns for the main app
+app_name = 'accounts'
+
+
+# setting up the URL patterns
+urlpattern = [
+     ]
