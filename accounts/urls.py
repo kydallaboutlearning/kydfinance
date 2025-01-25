449dcd1c05f614cda916f1b2cde07ee36f8bde89
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-01-25 10:13:19 +0100

    Feat(urls): Added urls for accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path,include
 from django.utils.translation import gettext_lazy as _
+from .views import DashboardView, register_view
 
                     
 
@@ -9,4 +10,8 @@ app_name = 'accounts'
 
 # setting up the URL patterns
 urlpattern = [
+     path(_('Accounts/'), include('django.contrib.auth.urls'),
+     path( _('DashBoard/'), DashboardView, name = 'dashboard')),
+     path(_('register/'), register_view, name = 'register'),
      ]
+     
