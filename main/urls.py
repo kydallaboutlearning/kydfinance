15bcca1970d6efc6ec2552ea38a940e4a3142162
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-01-25 10:19:08 +0100

    Refactor(urls): removed the accountsurls from the main app

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
 from django.utils.translation import gettext_lazy as _
-from .views import LandingPageView,DashboardView
+from .views import LandingPageView
 
 
 # URL patterns for the main app
@@ -10,6 +10,4 @@ app_name = 'main'
 # setting up the URL patterns
 urlpatterns = [
      path('', LandingPageView, name='landing-page'),
-     path(_('Accounts/'),include('django.contrib.auth.urls'),
-     path( _('DashBoard/'),DashboardView, name = 'dashboard')),
-    ]
+              ]
