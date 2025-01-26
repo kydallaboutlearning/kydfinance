76964f82346d75b57228e062acab12272b69c08b
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-01-26 19:54:40 +0100

    Chore(Accounts): added urls forthe logoutform  view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
 from django.utils.translation import gettext_lazy as _
-from .views import DashboardView, register_view
+from .views import DashboardView, register_view, LogoutView
 
                     
 
@@ -13,5 +13,6 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),  
     path(_('DashBoard/'), DashboardView, name='dashboard'),  
     path(_('register/'), register_view, name='register'),
+    path(_('logout-form/'), LogoutView, name='logout-form'),
 ]
      
