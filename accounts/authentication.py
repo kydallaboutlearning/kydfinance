6f64a07ad63e8e8a69f3078912856163c3ee272e
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-01-27 15:12:01 +0100

    Chore(Accounta): added authentication to allow profile ctreration via google account

---
+++ accounts/authentication.py
@@ -0,0 +1,8 @@
+from accounts.models import Profile
+
+
+def create_profile(backend, user, *args, **kwargs):
+ """
+ Create user profile for social authentication
+ """
+ Profile.objects.get_or_create(user=user)
