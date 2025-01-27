16f64eae7a7447b07db42e767f5d6756e5d31d4d
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-01-27 01:18:50 +0100

    Feat(Accounts_: Added the profile to the admin using translatableadmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Profile
+from parler.admin import TranslatableAdmin
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileModel(TranslatableAdmin):
+    list_display = ['user', 'date_of_birth', 'phone_number', 'display_bio', 'created', 'last_login']
+    list_editable = ['phone_number']  # Ensure this is in list_display
+    search_fields = ['user__username', 'user__email', 'phone_number']  # Add search functionality
+    list_filter = ['created', 'last_login']  # Filters for admin panel
+
+    def display_bio(self, obj):
+        """Display the translated bio in the list view."""
+        return obj.bio  # Parler handles translations automatically
+    display_bio.short_description = 'Bio'  # Admin column title
