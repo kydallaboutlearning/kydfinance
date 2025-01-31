46fd556819fd978c9749edd51f32cad3388320e1
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-01-31 16:56:03 +0100

    Feat(Blog-views): added the get post list for the home page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import BlogPost
 
 # Create your views here.
+
+def post_list_by(request):
+    posts = BlogPost.Published.all()[:3]
+    return render(
+        request,
+        'main/home.html',
+        {'posts':posts}
+    )
+    
