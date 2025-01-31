62993974b8a15f395ead5d71a8d59e5cb3fc3ac2
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-01-31 16:17:02 +0100

    Chore(blog): Done with registering the admin

--- blog/admin.py
+++ blog/admin.py
@@ -4,10 +4,17 @@ from .models import BlogPost
 
 
 
+
+
 # Register your models here.
 @admin.register(BlogPost)
 class BlogAdmin(TranslatableAdmin):
     list_display = ['title','status','publish','created']
+    list_editable = ['title','status']
+    search_fields = ['title','status','publish']
+    date_hierachy  = 'publish'
+    ordering  = ['status','publish']
+    show_facet = admin.ShowFacets.Always
 
     
     
