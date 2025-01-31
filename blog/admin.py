0fb13a25e5a65cc2a35e347e3717094a9fdc2361
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-01-31 16:46:57 +0100

    Fix(blog): Added the right call for the title model

--- blog/admin.py
+++ blog/admin.py
@@ -2,15 +2,16 @@ from django.contrib import admin
 from parler.admin import TranslatableAdmin
 from .models import BlogPost
 
-
 # Register your models here.
 @admin.register(BlogPost)
 class BlogAdmin(TranslatableAdmin):
-    list_display = ['safe_title', 'status', 'publish', 'created']  # FIXED: Using safe_title
+    list_display = ['get_title', 'status', 'publish', 'created'] # Use get_title instead of 'safe_title'
     list_editable = ['status']
-    search_fields = ['translations__title', 'status', 'publish']  # FIXED: Parler requires translations__
-    date_hierarchy = 'publish'  # FIXED TYPO
-    ordering = ('status', '-publish')  # FIXED: Using a tuple
-
-    def get_prepopulated_fields(self, request, obj=None):
-        return {'slug': ('title',)}
+    search_fields = ['translations__title', 'status', 'publish']  #Use translations__title for search
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+    
+    def get_title(self, obj):
+        return obj.safe_translation_getter('title', default='[No Title]')  #  Fallback for missing translations
+    
+    get_title.short_description = 'Title'  # Set column header in admin panel
