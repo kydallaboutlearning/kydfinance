81295b02e31a3c601720ec60d4a76473b83270c1
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-01-31 16:07:04 +0100

    Chore(blog): done with the admin for the blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from parler.admin import TranslatableAdmin
+from .models import BlogPost
+
+
 
 # Register your models here.
+@admin.register(BlogPost)
+class BlogAdmin(TranslatableAdmin):
+    list_display = ['title','status','publish','created']
+
+    
+    
+    
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug': ('title',)}
+    
+    
