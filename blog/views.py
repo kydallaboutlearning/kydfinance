321cf11b28c9b0f567857f58c806a422f9ee44d5
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-01-31 21:14:26 +0100

    Chore(Blog:Views): added the class based for for the blog list

--- blog/views.py
+++ blog/views.py
@@ -12,4 +12,11 @@ def post_list_by(request):
         {'posts':posts}
     )
     
+class Post_list(ListView):
+    """Adding a general page view"""
+    model = BlogPost()
+    query_set = BlogPost.Published.all()
+    paginate_by = 5
+    context_object_name = 'posts'    
+
     
