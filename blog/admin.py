39e3dd996cb1c5baa90e016aee651585a0b002e3
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-01-31 16:42:36 +0100

    Chore(blog): added the admin for the blog

--- blog/admin.py
+++ blog/admin.py
@@ -3,23 +3,14 @@ from parler.admin import TranslatableAdmin
 from .models import BlogPost
 
 
-
-
-
 # Register your models here.
 @admin.register(BlogPost)
 class BlogAdmin(TranslatableAdmin):
-    list_display = ['title','status','publish','created']
-    list_editable = ['title','status']
-    search_fields = ['title','status','publish']
-    date_hierachy  = 'publish'
-    ordering  = ['status','publish']
-    show_facet = admin.ShowFacets.Always
+    list_display = ['safe_title', 'status', 'publish', 'created']  # FIXED: Using safe_title
+    list_editable = ['status']
+    search_fields = ['translations__title', 'status', 'publish']  # FIXED: Parler requires translations__
+    date_hierarchy = 'publish'  # FIXED TYPO
+    ordering = ('status', '-publish')  # FIXED: Using a tuple
 
-    
-    
-    
     def get_prepopulated_fields(self, request, obj=None):
         return {'slug': ('title',)}
-    
-    
