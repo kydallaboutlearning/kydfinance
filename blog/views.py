5e462ce230bcaf6d0bfa62d760c03d44ff013a11
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-02-02 21:38:57 +0100

    chore(blog: views): added views for the blog

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,40 @@
-from django.shortcuts import render
+# importing for list and detail view
+from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from django.core.paginator import 
 
 # Create your views here.
 
-def post_list_by(request):
-    posts = BlogPost.Published.all()[:3]
+def post_list_norms(request):
+    posts = BlogPost.published.all()
+        
     return render(
         request,
-        'main/home.html',
-        {'posts':posts}
+        'blog/post/list.html',
+        {'posts': posts}
     )
+
     
+
+# creating the class post view
 class Post_list(ListView):
     """Adding a general page view"""
     model = BlogPost()
-    query_set = BlogPost.Published.all()
+    query_set = BlogPost.published.all()
     paginate_by = 5
     context_object_name = 'posts'    
 
+
+def Post_Detail(request,year,month,day,post):
+    # get the post based on the requested data in frm of year, day,
+    post = get_object_or_404(
+        BlogPost,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
+    
     
