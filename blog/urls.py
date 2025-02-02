ac52233583ae65aea09c7e149ad9f75cee4c0238
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-02-02 21:37:54 +0100

    Chore(Blogs: urls): added urls for the blog views

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.utils.translation import gettext_lazy as _
+from .views import *
 
                     
 
@@ -9,4 +10,7 @@ app_name = 'blog'
 
 # setting up the URL patterns
 urlpattern = [
+     '', post_list_norms, name ='post_list',
+     'post_class/', Post_list.as_view(), name = 'post_list_class',
+     
      ]
