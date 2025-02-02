7edc6380e370448d4a1eb06d8a7b03b1f50f1261
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-02-02 21:35:11 +0100

    Fix(Blog: admin): changed the fields to titile

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,7 @@ from .models import BlogPost
 # Register your models here.
 @admin.register(BlogPost)
 class BlogAdmin(TranslatableAdmin):
-    list_display = ['get_title', 'author','status', 'publish', 'created','updated'] # Use get_title instead of 'safe_title'
+    list_display = ['title', 'author','status', 'publish', 'created','updated'] # Use get_title instead of 'safe_title'
     list_editable = ['status','author']
     search_fields = ['translations__title', 'status', 'publish']  #Use translations__title for search
     date_hierarchy = 'publish'
@@ -13,11 +13,11 @@ class BlogAdmin(TranslatableAdmin):
     show_facets = admin.ShowFacets.ALWAYS
     
     def get_prepopulated_fields(self, request, obj=None):
-        return {'slug': ('get_title',)}
+        return {'slug': ('title',)}
 
     
     
     def get_title(self, obj):
         return obj.safe_translation_getter('title', default='[No Title]')  #  Fallback for missing translations
     
-    get_title.short_description = 'Title'  # Set column header in admin panel
+    get_title.short_description = 'title'  # Set column header in admin panel
