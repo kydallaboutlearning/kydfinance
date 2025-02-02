b7a174574805948305c94b840cf319a4c45979f3
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-02-02 21:42:06 +0100

    Feat(Main:template): added blog to the main view

--- main/views.py
+++ main/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render,redirect
 from django.utils.translation import get_language
+from blog.models import BlogPost
 
 # Create your views here.
 
 
 def LandingPageView(request):
+    posts = BlogPost.published.all()[:3]
     current_language = get_language()
 
     context = {
-        'current_language': current_language
+        'current_language': current_language,
+        'posts':posts,
     }
     return render(request, 'main/home.html', context)
 
