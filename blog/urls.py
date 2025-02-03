5e0b9135e4f9a95faa4185ae7cdcf5d2fd37da1f
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-02-03 14:30:42 +0100

    Chore(Blog: urls): added canonical urls for blog details

--- blog/urls.py
+++ blog/urls.py
@@ -9,13 +9,8 @@ app_name = 'blog'
 
 
 # setting up the URL patterns
-urlpattern = [
+urlpatterns = [
      path('', post_list_norms, name ='post_list'),
      path('post_class/', Post_list.as_view(), name = 'post_list_class'),
-     path(
-        '<int:year>/<int:month>/<int:day>/<slug:post>/',
-        views.post_detail,
-        name='post_detail',
-          ),
-     
+     path('<int:year>/<int:month>/<int:day>/<slug:post>/', Post_Detail, name='post_detail',),
      ]
