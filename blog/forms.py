1485991112641e9b58447602a1dfc582cf2e4d06
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-02-06 02:49:56 +0100

    Chore(blog: forms): added the forms for newsletter

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,10 @@
 from django import forms
-from .models import Newsletter
 from django.utils.translation import gettext_lazy as _
 
 
 class NewsletterForm(forms.Form):
-        email = forms.EmailField()
+        email = forms.EmailField(max_length=200,widget=forms.EmailInput(attrs={
+                                  "label":_("Email"),
+                                  "required":True,
+                                  "type":"email",
+                                  "placeholder":_("Enter your Email")}))
