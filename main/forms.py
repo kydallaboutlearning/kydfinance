956c147425c0ddca627f8b586bc8695c3cdfc90c
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-02-06 19:45:35 +0100

    Refactor(

--- main/forms.py
+++ main/forms.py
@@ -1,15 +1,28 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
 from .models import Newsletter
+import logging
+from django import forms
+
+
+
+# Configure logger
+logger = logging.getLogger(__name__)
 
 
 class NewsletterSub(forms.Form):
-        email = forms.EmailField()
-        # A funstion to identify if  it's an emailor username
+        
+        email = forms.EmailField(max_length=200,widget=forms.EmailInput(attrs={
+                                  "label":_("Email"),
+                                  "required":True,
+                                  "type":"email",
+                                  "placeholder":_("Enter your Email")}))
+
         def clean_email(self):
-                context = self.cleaned_data
-                email = context.get('email')
+                email = self.cleaned_data.get('email')
 
                 if Newsletter.objects.filter(email=email).exists():
+                        logger.info(f"Duplicate email attempt: {email}")  # Log before raising error
                         raise forms.ValidationError(_('Email already exists'))
+
                 return email
