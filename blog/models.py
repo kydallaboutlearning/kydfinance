8388a5da4a31812797583a4ca8094015a30b790b
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-02-10 11:18:17 +0100

    Chore(bl0og:models): changed the stringt return for my blogs

--- blog/models.py
+++ blog/models.py
@@ -119,7 +119,7 @@ class Comment(TranslatableModel):
 
     def __str__(self):
         note = _("Comment by")
-        return f"{note}: {self.profile} - {self.safe_translation_getter('body', default=_('[No Body]'))} on {self.post}"
+        return f"{note}: {self.profile} - {self.safe_translation_getter('body')} on {self.post}"
 
 
 
