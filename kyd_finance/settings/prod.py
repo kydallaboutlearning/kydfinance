8c80c42cd9ffc90d487f46bb4f4f4c81f02832fc
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-02-17 11:44:41 +0100

    reset the logs

--- kyd_finance/settings/prod.py
+++ kyd_finance/settings/prod.py
@@ -33,20 +33,3 @@ DATABASES = {
 }
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
