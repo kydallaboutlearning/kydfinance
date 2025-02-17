85b6a8a53dc29e3d63aa07bc49c033a7e0ce25da
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-02-17 01:26:16 +0100

    Updatee: optimized for waitress

--- kyd_finance/wsgi.py
+++ kyd_finance/wsgi.py
@@ -11,6 +11,20 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kyd_finance.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kyd_finance.settings.prod')
 
 application = get_wsgi_application()
+
+import os
+from django.core.wsgi import get_wsgi_application
+from waitress import serve
+
+# Set the default settings module for the 'django' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+
+# Get the WSGI application for your Django project
+application = get_wsgi_application()
+
+if __name__ == "__main__":
+    # Use Waitress to serve the application
+    serve(application, host='0.0.0.0', port=8000)
